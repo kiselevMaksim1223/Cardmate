067700e735bf11c5678bedae47cef85ff36eb823
Maksim_Kiselev <laurkurt@gmail.com>
2023-05-19 00:28:35 +0300

    add env

--- src/api/instans-api.ts
+++ src/api/instans-api.ts
@@ -6,7 +6,7 @@ export type ResponseType<T = {}> = {
 }
 
 export const instance = axios.create({
-  baseURL: process.env.REACT_APP_BACK_URL || process.env.REACT_APP_BACK_URL_DEV,
+  baseURL: process.env.REACT_APP_BACK_URL_PROD || process.env.REACT_APP_BACK_URL_DEV,
   // baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:7542/2.0/' : 'https://neko-back.herokuapp.com/2.0/',
   withCredentials: true,
 })
